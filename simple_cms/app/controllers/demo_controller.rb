bec2e1bfaae041ce628bc7848435104eaaf7bb6f
Travis H <t3patterson@gmail.com>
2015-08-28 10:01:18 -0500

    setting up the rails app

---
+++ simple_cms/app/controllers/demo_controller.rb
@@ -0,0 +1,12 @@
+
+#inherits from ApplicationController
+class DemoController < ApplicationController
+  
+  layout false
+
+   # simple method (an action that the controller will perform)
+   #  by default: it renders the view and gives us a layout
+  def index 
+  end
+
+end
