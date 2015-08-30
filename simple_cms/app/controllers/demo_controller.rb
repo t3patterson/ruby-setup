314bfcedd1c8d27dbf5f427ea79c1cc0c023cd56
Travis H <t3patterson@gmail.com>
2015-08-29 19:27:40 -0500

    router-controller-view structure and dynamic ruby inside of views

--- simple_cms/app/controllers/demo_controller.rb
+++ simple_cms/app/controllers/demo_controller.rb
@@ -4,9 +4,36 @@ class DemoController < ApplicationController
   
   layout false
 
-   # simple method (an action that the controller will perform)
-   #  by default: it renders the view and gives us a layout
+   # these are the Actions 
+   #  (a method that the controller will 
+   #    execute based on the route)
+  
+  #matches the :action portion of the route
   def index 
+  	# do some shit....
+  	
+
+  	# ...THEN as a default behavior, 
+  	#     render the `index.html.erb` template
+  	#     that matches the route
+  end
+
+  def hello
+    #however, we can change which template to render
+  	render('demo/lost-in-time')
+  	  # will render `.../demo/lost-in-time.html.erb  
+  end
+
+  def dynamic_ruby
+    render('dynamic-ruby')
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'durrr-durr')
+  end
+
+  def send_me_away
+  	redirect_to("http://deadspin.com/")
   end
 
 end
